7299bb80e903d9a060982d6571ce8cb1eb32a397
GBR1405 <124008178+GBR1405@users.noreply.github.com>
2025-07-24 15:30:30 -0600

    Errorcirot jiji

--- Job-Portal-Backend/src/app.js
+++ Job-Portal-Backend/src/app.js
@@ -1599,7 +1599,7 @@ socket.on('initHangmanGame', ({ partidaId, equipoNumero }) => {
 // Evento para adivinar letra
 socket.on('guessLetter', ({ partidaId, equipoNumero, letra }) => {
   try {
-    const gameId = `hangman-${partidaId}-${equipoNumero}`;
+    const gameId = `hangman-${partidaId}-${equipoNumero}-${config.currentIndex}`;
     const game = hangmanGames[gameId];
     
     if (!game) throw new Error('Juego no encontrado');
