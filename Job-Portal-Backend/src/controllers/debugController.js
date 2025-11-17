4e5e0fadef239cc1a94fad3668f20656798b66dd
GBR1405 <124008178+GBR1405@users.noreply.github.com>
2025-11-17 14:39:53 -0600

    3° intento

--- Job-Portal-Backend/src/controllers/debugController.js
+++ Job-Portal-Backend/src/controllers/debugController.js
@@ -11,8 +11,8 @@ dotenv.config();
 // Configuración del transporter para correos (similar a authController)
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
-  port: parseInt(process.env.EMAIL_PORT),
-  secure: false, 
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
