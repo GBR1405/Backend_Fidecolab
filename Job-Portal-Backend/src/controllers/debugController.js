6cff813879dee0ec4186e8bdb8c2cd72f0239585
GBR1405 <124008178+GBR1405@users.noreply.github.com>
2025-11-17 14:10:19 -0600

    2° prueba

--- Job-Portal-Backend/src/controllers/debugController.js
+++ Job-Portal-Backend/src/controllers/debugController.js
@@ -11,13 +11,14 @@ dotenv.config();
 // Configuración del transporter para correos (similar a authController)
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
-  port: 2525,
-  secure: false,
+  port: process.env.EMAIL_PORT,
+  secure: false, // porque usamos 587
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
   tls: {
+    ciphers: "SSLv3",
     rejectUnauthorized: false,
   },
 });
--- Job-Portal-Backend/src/controllers/teacherController.js
+++ Job-Portal-Backend/src/controllers/teacherController.js
@@ -14,13 +14,14 @@ import { GenerarBitacora } from "../controllers/generalController.js";
 
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
-  port: 2525,
-  secure: false,
+  port: process.env.EMAIL_PORT,
+  secure: false, 
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
   tls: {
+    ciphers: "SSLv3",
     rejectUnauthorized: false,
   },
 });
